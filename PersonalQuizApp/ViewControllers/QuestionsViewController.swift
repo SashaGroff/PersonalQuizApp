bfee216ef51ae46eb0d121a5778e36d0bd8b8f59
SashaGroff <148804078+SashaGroff@users.noreply.github.com>
2023-12-02 16:49:00 +0300

    Work with UI

---
+++ PersonalQuizApp/ViewControllers/QuestionsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PersonalQuizApp
+//
+//  Created by Александр Гончаров on 02.12.2023.
+//
+
+import UIKit
+
+final class QuestionsViewController: UIViewController {
+    
+    private let questions = Question.getQuestion()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
